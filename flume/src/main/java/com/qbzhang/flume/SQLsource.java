5eddb37256c4cc6f63a76a5fcc7e524645a5091b
qbzhang <970401732@qq.com>
2018-10-18 15:14:34 +0800

    Initial commit

---
+++ flume/src/main/java/com/qbzhang/flume/SQLsource.java
@@ -0,0 +1,8 @@
+package com.qbzhang.flume;
+
+/**
+ * @author qbzhang
+ * @time 2018-10-16 21:30
+ */
+public class SQLsource extends  {
+}
---
+++ gittest/src/main/java/com/qbzhang/git/bean/GitTest.java
@@ -0,0 +1,14 @@
+package com.qbzhang.git.bean;
+
+/**
+ * @author qbzhang
+ * @time 2018-10-18 14:42
+ */
+public class GitTest {
+    private String name = "gittest";
+    private String version = "1.0.0";
+
+    public String instruction() {
+        return "This is my first gittest on idea";
+    }
+}
